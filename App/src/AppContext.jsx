acfa5a9d426a77dd616710ce5e14d4599961d4d4
CyberSentinelLabs <cybersentinelaix@gmail.com>
2025-09-04 01:26:28 +0100

    Fixes

--- App/src/AppContext.jsx
+++ App/src/AppContext.jsx
@@ -1,4 +1,4 @@
 import { createContext } from "react";
 
-// Create a context for the application's state
+// Create context for the application's state
 export const AppContext = createContext();
--- App/src/components/ResultsPage.jsx
+++ App/src/components/ResultsPage.jsx
@@ -9,11 +9,26 @@ const ResultsPage = () => {
       <p className="page-subtitle">
         {" "}
         There are no results to display.
-        <br /> Upload a file, make a video recording or an audio recording to see
-        results.
+        <br /> Upload a file, make a video recording or an audio recording to
+        see results.
       </p>
 
       <p className="page-subtitle"></p>
+
+      <div className="button-group">
+        <button
+          onClick={() => setPage("upload")}
+          className="main-action-button primary"
+        >
+          Upload
+        </button>
+        <button
+          onClick={() => setPage("record")}
+          className="main-action-button secondary"
+        >
+          Record
+        </button>
+      </div>
       <button onClick={() => setPage("home")} className="nav-button">
         Back
       </button>
